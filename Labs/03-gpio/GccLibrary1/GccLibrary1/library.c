c8f0c1dae6318c421a2ddfd5bfca2b477783b678
xczmel00 <xczmel00@vut.cz>
2021-10-13 13:05:23 +0200

    ...

---
+++ Labs/03-gpio/GccLibrary1/GccLibrary1/library.c
@@ -0,0 +1,16 @@
+/*
+ * GccLibrary1.c
+ *
+ * Created: 06.10.2021 15:36:54
+ * Author : student
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ Labs/03-gpio/GccLibrary2/GccLibrary2/library.c
@@ -0,0 +1,16 @@
+/*
+ * GccLibrary2.c
+ *
+ * Created: 06.10.2021 15:38:39
+ * Author : student
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
